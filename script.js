d3a1fb8b71fa242c059a439cc32b8c7e997ff2a6
yuliia <pustovayajulia@gmail.com>
2023-08-08 22:40:16 +0300

    Add logic for nested arrays

--- script.js
+++ script.js
@@ -29,13 +29,17 @@ function generateNodesSimple(el, ulPar) {
 function generateNodes(arr, ul) {
   let ulInternal = document.createElement("ul");
   arr.forEach((value) => {
-    generateNodesSimple(value, ulInternal);
+    if (!Array.isArray(value)) {
+      generateNodesSimple(value, ulInternal);
+    } else {
+      generateNodes(value, ulInternal);
+    }
   });
   ul.append(ulInternal);
 
 }
 
-const exampleArr = [10, 12, [13, 10, 1], ['a', 'b'], 15];
+const exampleArr = [11, 12, [13, [10, 1]], ['a', 'b'], 15];
 console.log(exampleArr);
 let ulMajor = document.createElement("ul");
 acceptValues(exampleArr, ulMajor);
